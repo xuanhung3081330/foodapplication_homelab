6251a7cd6ccac60b5beb62262c6191beb624bae6
xuanhung <xuanhung3081330@gmail.com>
2025-06-13 14:47:48 +0900

    Refactor the connection string to read the info from file .env

--- app/config/db.go
+++ app/config/db.go
@@ -2,13 +2,32 @@ package config
 
 import (
 	"database/sql"
+	"log"
+	"os"
+	"fmt"
+
+	"github.com/joho/godotenv"
 	_ "github.com/lib/pq"
 )
 
 var DB *sql.DB
 
 func ConnectDB() error {
-	connStr := ""
+	// Load .env file
+	err := godotenv.Load(".env")
+	if err != nil{
+		log.Fatalf("Error loading .env file. Detail: %s", err.Error())
+	}
+
+	// Get values from environment
+	host := os.Getenv("DB_HOST")
+	port := os.Getenv("DB_PORT")
+	user := os.Getenv("DB_USER")
+	password := os.Getenv("DB_PASSWORD")
+	db_name := os.Getenv("DB_NAME")
+	ssl := os.Getenv("DB_SSLMODE")
+
+	connStr := fmt.Sprintf("host=%s port=%s user=%s password=%s dbname=%s sslmode=%s", host, port, user, password, db_name, ssl)
 	db, err := sql.Open("postgres", connStr)
 	if err != nil {
 		return err
